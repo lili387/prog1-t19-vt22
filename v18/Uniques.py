7618d4313397b26db2146ef30b626fb338192bf0
Linus Lind <54626126+lili387@users.noreply.github.com>
2022-05-03 09:25:10 +0200

    Snabbare unique uppslag

--- v18/Uniques.py
+++ v18/Uniques.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 # 1 downloaded from https://github.com/almgru/svenska-ord.txt/blob/master/svenska-ord.txt
 # 2 downloaded from https://github.com/titoBouzout/Dictionaries/blob/master/Swedish.dic
@@ -14,9 +15,8 @@ def read_dictionary_txt_as_list(filename):
             if "-" in word_part or " " in word_part:
                 continue
 
-            is_only_alpha = re.match('^[a-zåäö]+$', word_part)
-            if not is_only_alpha:
-                print(f'found strange {word_part}')
+            is_only_letters = re.match('^[a-zåäö]+$', word_part)
+            if not is_only_letters:
                 continue
 
             words.append(word_part)
@@ -24,11 +24,18 @@ def read_dictionary_txt_as_list(filename):
 
 word_list_1 = read_dictionary_txt_as_list(swedish_dict_filename_1)
 word_list_2 = read_dictionary_txt_as_list(swedish_dict_filename_2)
+word_set_2 = set(word_list_2)
 
 unique_words = []
 
+start = datetime.now()
+n = 1
 for word1 in word_list_1:
-    if not word1 in word_list_2:
+    if not word1 in word_set_2:
         unique_words.append(word1)
-    
+
+    n += 1
+    if n % 20000 == 0:
+        print(f'De första {n} tog {datetime.now()-start}')
+
 print(f'Found {len(unique_words)} unique words in list 1.')
